9b8f8f980916fab133236bbb402ea7adfb17e6a7
Sunil Gowda S <64064177+sunil3gs98@users.noreply.github.com>
2024-07-09 12:19:15 +0530

    K8_installation_master&worker.sh

--- K8_installation_master&worker.sh
+++ K8_installation_master&worker.sh
@@ -1,4 +1,4 @@
-#On Master and Worker Node
+
 
 sudo apt-get update
 sudo apt install docker.io -y
@@ -11,14 +11,4 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 sudo apt update
 sudo apt install -y kubeadm=1.28.1-1.1 kubelet=1.28.1-1.1 kubectl=1.28.1-1.1
 sudo apt install -y kubeadm=1.28.1-1.1 kubelet=1.28.1-1.1 kubectl=1.28.1-1.1
-
-
-#On Master Node Only
-
-1.sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-2.mkdir -p $HOME/.kube
-3.sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-4.sudo chown $(id -u):$(id -g) $HOME/.kube/config
-5.kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-6.kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.49.0/deploy/static/provider/baremetal/deploy.yaml
 
