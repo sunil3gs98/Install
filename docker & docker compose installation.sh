72d27e8f035fece6cbbd40e7cdd8691109ad3320
Sunil Gowda S <64064177+sunil3gs98@users.noreply.github.com>
2024-07-10 13:33:17 +0530

    docker compose installation.sh

--- docker & docker compose installation.sh
+++ docker & docker compose installation.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -8,8 +8,8 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo apt install docker-compose
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt install docker-compose -y
 service docker restart
 sudo usermod -aG docker $USER
 newgrp docker
